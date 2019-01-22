bfefcf7125d91892d4c69d36afd64f3fe221a6d8
hsdyyd <yidong06@sina.com>
2019-01-22 13:55:11 +0800

    init

---
+++ src/main/java/com/train/springboot/springboot/Springboot04WebRestfulcrudApplication.java
@@ -0,0 +1,14 @@
+package com.train.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04WebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot04WebRestfulcrudApplication.class, args);
+    }
+
+}
+
